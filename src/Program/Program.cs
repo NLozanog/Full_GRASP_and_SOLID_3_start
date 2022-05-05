9994327aff43d2944c6ad76d59a406773db28bfd
NLozanog <nicolas.lozano.282002@gmail.com>
2022-05-05 20:54:08 -0300

    Update Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -26,11 +26,10 @@ namespace Full_GRASP_And_SOLID
             recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
             recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
 
-            IPrinter printer;
-            printer = new ConsolePrinter();
-            printer.Print(recipe);
-            printer = new FilePrinter();
-            printer.Print(recipe);
+            ConsolePrinter consolePrinter = new ConsolePrinter();
+            FilePrinter filePrinter = new FilePrinter();
+            consolePrinter.Print(recipe);
+            filePrinter.Print(recipe);
         }
 
         private static void PopulateCatalogs()
